ec76804956286730d28e5066ac5bb70a62c2e055
Tarfful <beyonrus2003@gmail.com>
2024-03-09 11:17:08 +0400

    site_v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header/>
+      <Content/>
+			<Footer/>
     </div>
   );
 }
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -6,7 +6,7 @@ import logo from "./proper.jpg"
 export default class Header extends Component {
     render() {
         return (
-            <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/" >
                         <img
@@ -19,17 +19,18 @@ export default class Header extends Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="me-auto">
                             <Nav.Link href="/"> Home </Nav.Link>
                             <Nav.Link href="/about"> About us </Nav.Link>
                             <Nav.Link href="/contacts"> Contacts </Nav.Link>
                             <Nav.Link href="/blog"> Blog </Nav.Link>
+                            <Nav.Link href="/sandbox"> SandBox </Nav.Link>
                         </Nav>
-                        <Form inline>
+                        <Form className='d-flex'>
                             <FormControl
                                 type="text"
                                 placeholder="Search"
-                                className="mr-sm-2"
+                                className="me-2"
                             />
                             <Button variant="outline-info">Search</Button>
                         </Form>
